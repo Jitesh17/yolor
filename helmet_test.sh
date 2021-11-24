b0f9f8ffad25713c5e6d8c0e3bd0dfe2ed7a8ff5
Jitesh Gosar <gosar95@gmail.com>
2021-11-24 20:50:21 +0900

    Updates hyperparameters, augmentations

--- helmet_test.sh
+++ helmet_test.sh
@@ -1,5 +1,5 @@
-python yolor/test.py --data data/coco_helmet.yaml \
+python test.py --data data/coco_helmet.yaml \
 --img 1280 --batch 1 \
 --conf 0.25 --iou 0.65 --device 0 \
---cfg yolor/cfg/yolor_p6.cfg --weights runs/train/yolor_p6_1280_h1/weights/best.pt \
---name yolor_p6_val1 --names yolor/data/coco_helmet.names
+--cfg cfg/yolor_p6.cfg --weights runs/train/yolor_p6_1280_h1/weights/best.pt \
+--name yolor_p6_val1 --names data/coco_helmet.names
--- helmet_train.sh
+++ helmet_train.sh
@@ -1,7 +1,7 @@
-python yolor/train.py --batch-size 1 \
---img 1280 1280 --data data/coco_helmet.yaml --cfg yolor/cfg/yolor_p6_helmet.cfg \
+python train.py --batch-size 1 \
+--img 1280 1280 --data data/coco_helmet.yaml --cfg cfg/yolor_p6_helmet.cfg \
 --weights 'runs/train/yolor_p6_1280_h1/weights/best.pt' --device 0 \
---name yolor_p6_1280_h2 --hyp yolor/data/hyp.finetune.1280.yaml \
---epochs 50
+--name yolor_p6_1280_h2 --hyp data/hyp.helmet.1280.yaml \
+--epochs 30
 
-# yolor/yolor_p6.pt
+# yolor_p6.pt
